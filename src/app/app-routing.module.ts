acef47fc365a7182163d2d8d113b7652d59652a6
arom-danelli <aromdanelli@hotmail.com>
2024-05-26 11:05:11 -0300

    arrumei alguns pontos do front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ApiCreateComponent } from './components/api-create/api-create.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
